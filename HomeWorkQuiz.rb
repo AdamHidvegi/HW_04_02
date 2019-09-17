194d9b6d1c985d4f4159dad8db16ef0cbe62f759
AdamHidvegi <hidvegiadam@gmail.com>
2019-09-18 00:30:49 +0100

    homework week_04 day_02

---
+++ HomeWorkQuiz.rb
@@ -0,0 +1,50 @@
+## Homework Review Quiz
+1. What is the name of the Ruby web framework that we've been using to create web apps? (1)
+
+    (Frank) Sinatra
+
+2. What do HTTP and URL stand for? (2)
+
+    HTTP: HyperText Transfer Protocol
+    URL: Uniform Resource Locator
+
+3. Which HTTP verb is used for retrieving data? (1)
+
+    get
+
+4. Which two Rs describe the client/server data cycle? (1)
+
+    Request - Response
+
+5. Which component regulates interaction between our models and views? (1)
+
+    M - V - C.ontroller
+
+6. What might we find stored in 'params'? (1)
+
+    Params is the short version of parameters. Params is a hash of key/value pairs that comes from the browser. So we can store additional data in it.
+
+7. How do you make a variable declared in a controller available to a view? (1)
+
+    Using the <%= %> symbols
+
+8. What's the difference between these tags?
+<%= %> and <% %> (1)
+
+    The first pair will execute the Ruby code in it and you will see the result(output) on your website, the second pair will do the same, but you won't see the result, it's gonna be thrown away.
+
+9. Which keyword do we use in our layout.erb to execute a page template? (1)
+
+    yield
+
+10. A server error might correspond to which status code?(1)
+
+    5xx
+
+11. What does JSON stand for and when might we use it? (1)
+
+    JSON: JavaScript Object Notation, we can use it for data-interchanging? Unfortuntely I don't know the answer for this question.
+
+12. Draw a cycle in the correct order from these terms: [Model, Controller, View, Client]  (1)
+
+    In theory the picture is right next to this file in the repository.
